0590815fc24bc5800b6eba48c0c437ecf84223a2
kurockett <cxmdeluxe@yandex.by>
2021-02-02 21:17:32 +0300

    feat: доделан gulp, добавлены шрифты и проверка на наличие webpcss в браузере

---
+++ analytics/#src/js/script.js
@@ -0,0 +1,1 @@
+@@include('webpcss.js')
--- analytics/gulpfile.js
+++ analytics/gulpfile.js
@@ -11,7 +11,13 @@ const {dest, src} = require('gulp'),
     uglify = require('gulp-uglify-es').default,
     imagemin = require('gulp-imagemin'),
     webp = require('gulp-webp'),
-    webpHTML = require('gulp-webp-html')
+    webpHTML = require('gulp-webp-html'),
+    webpCSS = require('gulp-webp-css'),
+    svgSprite = require('gulp-svg-sprite'),
+    ttf2woff = require('gulp-ttf2woff'),
+    ttf2woff2 = require('gulp-ttf2woff2'),
+    fonter = require('gulp-fonter'),
+    fs = require('fs')
 
 const projectFolder = 'dist',
     sourceFolder = '#src'
@@ -51,8 +57,8 @@ const browserSync = (params) => {
 }
 
 const html = () => src(path.src.html)
-    .pipe(webpHTML())
     .pipe(fileInclude())
+    .pipe(webpHTML())
     .pipe(dest(path.build.html))
     .pipe(browsersync.stream())
 
@@ -60,6 +66,7 @@ const css = () => src(path.src.css)
     .pipe(scss({outputStyle: "expanded"}))
     .pipe(groupMedia())
     .pipe(autoprefixer({overrideBrowserslist: ['last 5 versions'], cascade: true}))
+    .pipe(webpCSS())
     .pipe(dest(path.build.css))
     .pipe(cleanCSS())
     .pipe(rename({extname: '.min.css'}))
@@ -89,6 +96,60 @@ const img = () => src(path.src.img)
     .pipe(dest(path.build.img))
     .pipe(browsersync.stream())
 
+const fonts = () => {
+    src(path.src.fonts)
+        .pipe(ttf2woff())
+        .pipe(dest(path.build.fonts))
+    return src(path.src.fonts)
+        .pipe(ttf2woff2())
+        .pipe(dest(path.build.fonts))
+}
+
+gulp.task('otf2ttf', () => src([sourceFolder + '/fonts/*.otf'])
+    .pipe(fonter({
+        formats: ['ttf']
+    }))
+    .pipe(dest(sourceFolder + '/fonts/'))
+)
+
+
+gulp.task('svgSprite', () =>
+    gulp.src([sourceFolder + '/iconsprite/*.svg'])
+        .pipe(svgSprite({
+            mode: {
+                stack: {
+                    sprite: '../icons/icons.svg',
+                    example: true
+                }
+            }
+        }))
+        .pipe(dest(path.build.img))
+)
+
+const fontsStyle = () => {
+    let fileContent = fs.readFileSync(sourceFolder + '/scss/_fonts.scss');
+    if (fileContent == '') {
+        fs.writeFile(sourceFolder + '/scss/_fonts.scss', '', cb);
+        return fs.readdir(path.build.fonts, function (err, items) {
+            if (items) {
+                let cFontname;
+                for (var i = 0; i < items.length; i++) {
+                    let fontname = items[i].split('.');
+                    fontname = fontname[0];
+                    if (cFontname !== fontname) {
+                        fs.appendFile(sourceFolder + '/scss/_fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
+                    }
+                    cFontname = fontname;
+                }
+            }
+        })
+    }
+}
+
+function cb() {
+
+}
+
 const watchFiles = () => {
     gulp.watch([path.watch.html], html)
     gulp.watch([path.watch.css], css)
@@ -98,14 +159,15 @@ const watchFiles = () => {
 
 const clean = () => del(path.clean)
 
-const build = gulp.series(clean, gulp.parallel(css, html, js, img))
+const build = gulp.series(clean, gulp.parallel(js, css, html, img, fonts), fontsStyle)
 const watch = gulp.parallel(build, watchFiles, browserSync)
 
-exports.html = html
-exports.css = css
-exports.js = js
+exports.fontsStyle = fontsStyle
+exports.fonts = fonts
 exports.img = img
-// exports.fonts = fonts
+exports.js = js
+exports.css = css
+exports.html = html
 exports.build = build
 exports.watch = watch
 exports.default = watch
